f7d39c2a62e4a333d6691e8db427f9d73e92abe1
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 00:43:28 +0530

    adding the temp_mail file

--- src/main/java/com/model/Temp_Mail.java
+++ src/main/java/com/model/Temp_Mail.java
@@ -1,8 +1,15 @@
 package com.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Temp_Mail {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String email_address;
 	private boolean isActive;
