2e923c98e73328a0c76efbbfd2f8dbfb5d4b0257
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 01:30:57 +0530

    addedd

--- src/main/java/temp_mail/temp_mail/App.java
+++ src/main/java/temp_mail/temp_mail/App.java
@@ -3,9 +3,12 @@ package temp_mail.temp_mail;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import com.model.Email_Inbox;
+import com.model.Temp_Mail;
 import com.utils.HBUtils;
 
 /**
+ * 
  * Hello world!
  *
  */
@@ -15,5 +18,12 @@ public class App
     {
         Session session=HBUtils.sf.openSession();
         Transaction tx=session.beginTransaction();
+        
+        Temp_Mail tm= new Temp_Mail();
+        
+        Email_Inbox e=new Email_Inbox();
+        
+        tx.commit();
+        session.close();
     }
 }
