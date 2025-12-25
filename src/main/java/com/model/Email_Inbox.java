d56428b36be57b54157926c99c9ea1a684136c83
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 00:47:37 +0530

    add email_inbox file in com.model

---
+++ src/main/java/com/model/Email_Inbox.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class Email_Inbox {
+	
+	private int id;
+	private int temp_email_id;
+	private String sender;
+	private String subject;
+	private String messege_body;
+	
+	
+
+}
