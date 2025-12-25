ce8928104b0e1566190e24d2b1563e49db574025
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 00:45:35 +0530

    added getters and setters of the temp_mail file

--- src/main/java/com/model/Temp_Mail.java
+++ src/main/java/com/model/Temp_Mail.java
@@ -13,11 +13,27 @@ public class Temp_Mail {
 	private int id;
 	private String email_address;
 	private boolean isActive;
+	
 	@Override
 	public String toString() {
 		return "Temp_Mail [id=" + id + ", email_address=" + email_address + ", isActive=" + isActive + "]";
 	}
-	
-	
-
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail_address() {
+		return email_address;
+	}
+	public void setEmail_address(String email_address) {
+		this.email_address = email_address;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
 }
