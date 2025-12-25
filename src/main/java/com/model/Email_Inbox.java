78c14b45eb8ff1c9d2d4e3b55dd5d1d4839e5367
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 00:50:40 +0530

    add all changes in email_inbox

--- src/main/java/com/model/Email_Inbox.java
+++ src/main/java/com/model/Email_Inbox.java
@@ -16,5 +16,50 @@ public class Email_Inbox {
 	private String subject;
 	private String messege_body;
 	
+	@Override
+	public String toString() {
+		return "Email_Inbox [id=" + id + ", temp_email_id=" + temp_email_id + ", sender=" + sender + ", subject="
+				+ subject + ", messege_body=" + messege_body + "]";
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTemp_email_id() {
+		return temp_email_id;
+	}
+
+	public void setTemp_email_id(int temp_email_id) {
+		this.temp_email_id = temp_email_id;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessege_body() {
+		return messege_body;
+	}
+
+	public void setMessege_body(String messege_body) {
+		this.messege_body = messege_body;
+	}
+	
 }
