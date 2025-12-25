dd7ca9159edf3b395232481d4f31e2f0eb78caf4
Haridas250503 <haridasshinde314@gmail.com>
2025-12-26 00:49:26 +0530

    just changes in email_inbox

--- src/main/java/com/model/Email_Inbox.java
+++ src/main/java/com/model/Email_Inbox.java
@@ -1,13 +1,20 @@
 package com.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Email_Inbox {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private int temp_email_id;
 	private String sender;
 	private String subject;
 	private String messege_body;
-	
 	
 
 }
